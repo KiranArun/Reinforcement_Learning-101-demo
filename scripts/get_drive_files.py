3a97acdbac0259e15057f69b2a11bdbada6f4492
KiranArun <kironni@gmail.com>
2018-04-30 15:37:04 +0100

    removed google authentication for pydrive from scipts

--- scripts/get_drive_files.py
+++ scripts/get_drive_files.py
@@ -1,12 +1,8 @@
 import os, sys
 from pydrive.auth import GoogleAuth
 from pydrive.drive import GoogleDrive
-from google.colab import auth
 from oauth2client.client import GoogleCredentials
 
-
-
-auth.authenticate_user()
 gauth = GoogleAuth()
 gauth.credentials = GoogleCredentials.get_application_default()
 drive = GoogleDrive(gauth)
