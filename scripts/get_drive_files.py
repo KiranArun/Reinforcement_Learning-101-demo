b9802e0f8eea20f717f1fc7fa933596e87c0c304
KiranArun <kironni@gmail.com>
2018-04-30 15:00:11 +0100

    added get drive files and install dependancies scripts

---
+++ scripts/get_drive_files.py
@@ -0,0 +1,63 @@
+import os, sys
+from pydrive.auth import GoogleAuth
+from pydrive.drive import GoogleDrive
+from google.colab import auth
+from oauth2client.client import GoogleCredentials
+
+auth.authenticate_user()
+gauth = GoogleAuth()
+gauth.credentials = GoogleCredentials.get_application_default()
+drive = GoogleDrive(gauth)
+
+local_download_path = os.path.expanduser('./')
+
+if len(sys.argv) > 1:
+  drive_folder = sys.argv[1]
+else:
+  drive_folder = 'Reinforcement_Learning-101-demo'
+
+drive_folder = drive.ListFile({'q': "title = '"+str(drive_folder)"+'"}).GetList()
+
+ignore = ['.ipynb_checkpoints','__pycache__','core']
+
+to_download = drive_folder
+
+to_download[0]['path'] = local_download_path
+to_download[0]['title'] = ''
+
+def retrieve_folder_files(path_to_folder, folder):
+
+  files = drive.ListFile({'q': "'"+folder['id']+"' in parents"}).GetList()
+    
+  try:
+    os.makedirs(os.path.join(path_to_folder,folder['title']))
+  except: pass
+    
+  subdirs = []
+  
+  for file in files:
+      
+    if all(s not in file['title'] for s in ignore) and all(s not in path_to_folder for s in ignore):
+      
+      if file['mimeType'][-6:] != 'folder':
+                
+        fname = os.path.join(os.path.join(path_to_folder, folder['title']),file['title'])
+        print('Getting file:', fname)
+                
+        f_ = drive.CreateFile({'id': file['id']})
+        f_.GetContentFile(fname)
+      
+      else:
+        file['path'] = os.path.join(path_to_folder, folder['title'])
+        subdirs.append(file)
+             
+  return(subdirs)
+
+while len(to_download) > 0:
+  
+  subdirs = []
+
+  for folder in to_download:
+    subdirs += (retrieve_folder_files(folder['path'], folder))
+  
+  to_download = subdirs
---
+++ scripts/install_dependancies.sh
@@ -0,0 +1,8 @@
+apt update
+
+apt install cmake
+
+pip3 install gym
+pip3 install gym[atari]
+
+pip3 install 'opencv-contrib-python==3.3.0.9'
